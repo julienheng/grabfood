c8455be55429f448a9e3c7b973fc767b0c3e08c9
Elson Wong <elson.wong@live.com.sg>
2022-08-24 18:35:31 +0800

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 require 'faker'
 require 'date'
 
+puts "Clearing old data..."
 OrderItem.destroy_all
 Order.destroy_all
 Item.destroy_all
@@ -16,57 +17,73 @@ Restaurant.destroy_all
 User.destroy_all
 # --user--
 
-10.times do
+puts "Creating 3 sellers..."
+3.times do |index|
+  user = User.create!(
+    name: Faker::Name.name,
+    address: Faker::Address.full_address,
+    phone: Faker::PhoneNumber.cell_phone,
+    email: "seller#{index}@email.com",
+    password: "password123",
+    is_seller: true
+  )
 
-User.create(
-  name: Faker::Name.name,
-  address: Faker::Address.full_address,
-  phone: Faker::PhoneNumber.cell_phone,
-  email: Faker::Internet.email,
-  password: "password123",
-  is_seller: false
-)
-
-# puts "#{user.name} created..."
-rand(0..2).times do
-  Restaurant.create(
+  puts "  Creating restaurants for Seller #{index}..."
+  restaurant = Restaurant.create!(
     name: Faker::Restaurant.name,
     location: Faker::Address.street_name,
     category: Faker::Food.ethnic_category,
     opening_hour: rand(6..9),
     closing_hour: rand(14..23),
-    user_id: rand(1..10)
+    user: user
   )
 
-  # puts "#{restaurant.name} created for #{user.name}"
-  Order.create(
-    delivered: false,
-    delivery_date: DateTime.now + rand(10),
-    total_cost: Faker::Number.decimal,
-    user_id: rand(1..10)
-  )
-
-rand(10).times do
-  # puts "Item and Order created"
-  item = Item.create(
+  puts "    Creating items for restaurant...\n\n"
+  rand(2..5).times do
+    Item.create!(
       name: Faker::Food.dish,
       price: Faker::Number.decimal,
       description: Faker::Food.description,
-      restaurant_id: rand(1..10)
+      restaurant: restaurant
+    )
+  end
+end
+
+puts "Creating non sellers..."
+restaurants = Restaurant.all
+
+10.times do |index|
+  user = User.create!(
+    name: Faker::Name.name,
+    address: Faker::Address.full_address,
+    phone: Faker::PhoneNumber.cell_phone,
+    email: "user#{index}@email.com",
+    password: "password123",
+    is_seller: false
   )
 
-  OrderItem.create(
-    quantity: Faker::Number.decimal_part,
-    item: item,
-    item_id: rand(1..10),
-    order_id: rand(1..10)
-  )
-end
-end
+  puts "  Creating orders for user..."
+  # select a single restaurant for this order
+  restaurant = restaurants.sample
+
+  rand(1..3).times do
+    order = Order.create!(
+      delivered: [true, false].sample,
+      delivery_date: Date.today,
+      total_cost: rand(100),
+      user: user
+    )
+
+    OrderItem.create!(
+      quantity: rand(1..4),
+      item: restaurant.items.sample,
+      order: order
+    )
+  end
 end
 
 
-# puts "Seeding completed!"
+puts "Seeding completed!"
 # --------------------------------
 
 #--restaurants--
