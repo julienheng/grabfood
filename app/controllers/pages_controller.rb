a3486a495e6e976a589da4bcb4de5fcfddcf346c
julien heng <hengjchan@gmail.com>
2022-08-25 15:43:49 +0800

    checkoutpage is not yet finished

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home]
 
   def home
+    @restaurants = Restaurant.all
     if user_signed_in?
       render current_user.is_seller ? 'restaurants/index' : 'pages/home'
     end
