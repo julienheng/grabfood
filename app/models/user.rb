456e7960ba973e79dd5c20139ef9477b522b423a
sofia ngapani <withsofina@gmail.com>
2022-08-26 20:03:07 +0800

    seller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :orders
   has_many :restaurants
-  validates :name, uniqueness: true, presence: true
+  validates :name, presence: true
   validates :address, :phone, presence: true
   validates :email, confirmation: true, presence: true
   validates :password, confirmation: true, presence: true
